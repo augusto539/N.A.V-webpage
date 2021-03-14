2814b3c5654e60d4a023fcf0a179e70f37621155
augusto <augustoantonelli88@gmail.com>
2021-03-14 19:41:00 -0300

    new images and reworked organization of the documents

--- scripts/models.js
+++ scripts/models.js
@@ -79,7 +79,7 @@ function myMove(ID) {
 
 
 function kk(ID) {
-    document.getElementById("main_img").src = `../img/models/wallet_${ID}.png`
+    document.getElementById("main_img").src = `../images/models/mockuped_wallets/wallet_${ID}.png`
 
     var elem = document.getElementById("featured_wallet");   
     var pos = whidth;
